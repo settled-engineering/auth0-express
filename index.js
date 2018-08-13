66827ae51489fd67cca5493e0b2f3fdc74a0d3ca
Chiara <chiara@settled.co.uk>
2018-08-13 09:46:44 +0100

    #MASTER Adding catch with redirection

--- index.js
+++ index.js
@@ -27,12 +27,39 @@ function verifyToken(idToken, {
 	return userAuthInfo;
 };
 
-module.exports = function (idTokenLocator, config) {
+/** 
+ * The function which returns your id token from request object
+ * @name IdTokenLocator
+ * @function
+ * @param {Object} req the express request object
+ * @returns {String} id token that we want to decode and verify 
+*/
+
+/** 
+ * The function that returns the redirect url
+ * @name RedirectUrl
+ * @function
+ * @param {Object} req the express request object
+ * @param {Object} res the express response object
+ * @returns {String} the url where the user is redirected is the token is not verified 
+*/
+
+/**
+ * Middleware that verifies an JSON Web Token and redirects to a given url if the verification fails.
+ * @param {IdTokenLocator} idTokenLocator the function which returns your id token from request object
+ * @param {RedirectUrl} redirectUrl the function that returns the redirect url
+ * @param {Object} config 
+ */
+module.exports = function (idTokenLocator, redirectUrl, config) {
 
 	return function (req, res, next) {
 		const idToken = idTokenLocator(req);
 
-		req.userAuthInfo = verifyToken(idToken, config);
-		next();
+		try {
+			req.userAuthInfo = verifyToken(idToken, config);
+			next();
+		} catch (err) {
+			res.redirect(redirectUrl(req,res));
+		}
 	}
 }
